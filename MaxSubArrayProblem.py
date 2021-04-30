041103076546c426af6e285a16d4154d0aa8b4dc
MyzLi <38722464+MyzLi@users.noreply.github.com>
2021-04-30 12:11:29 +0200

    updated after water incident (fml)

--- MaxSubArrayProblem.py
+++ MaxSubArrayProblem.py
@@ -10,6 +10,8 @@ for row in reader:
 A = data[0] #10000 Entries
 B = data[1] #5000 Entries
 C = data[2] #1000 Entries
+#Change active array to A B or C to change for both algorithms
+ACTIVE_ARRAY = A
 
 #Kadanes Algorithm - the alleged best way to find maximum contiguous subarray
 def kadaneAlg(given_array):
@@ -34,12 +36,56 @@ def kadaneAlg(given_array):
         best_total = max(best_total, best_for_this_entry)
     #Output best results
     return best_total
- 
-#change active array to A B or C to change for both algorithms
-ACTIVE_ARRAY = A
+
+
+#Lowest middle and highest refers to the index of the array, not the value of the entry
+def divAndConqMiddle(given_array, lowest, middle, highest):
+    lowest_total = 0
+    total = 0
+
+    for i in range(middle, lowest-1, -1):
+        total = total + given_array[i]
+        if total > lowest_total:
+            lowest_total = total
+
+    total = 0
+    highest_total = 0
+
+    for i in range(middle+1, highest+1):
+        total = total + given_array[i]
+        if total > highest_total:
+            highest_total = total
+    
+    return lowest_total+highest_total
+
+
+def divAndConq(given_array, lowest, highest):
+    
+    if highest == lowest:
+        return given_array[lowest]
+
+    #Establish the middle of the array
+    middle = (lowest+highest)//2
+
+    #Recursive splitting to smaller and smaller subarrays to find the maximum subarray total for each
+    maxLows = divAndConq(given_array, lowest, middle)
+    maxHighs = divAndConq(given_array, middle+1, highest)
+
+    #Get maximum subarray total crossing the middle
+    maxMiddle = divAndConqMiddle (given_array, lowest, middle, highest)
+
+    #Output the best of the three
+    return max(maxLows,maxHighs,maxMiddle)
+
 
 start_time = time.time()
 print("Maximum contiguous subarray total for A, according to Kadane's, is:", kadaneAlg(ACTIVE_ARRAY))
 end_time = time.time()
 elapsed_time = (end_time-start_time)*1000
-print("Running time: " , f'{elapsed_time:.9f}')
+print("With a running time of: " , f'{elapsed_time:.9f}',"ms")
+
+start_time = time.time()
+print("Maximum contiguous subarray total for A, according to Divide-And-Conquer, is:", divAndConq(ACTIVE_ARRAY, 0, len(ACTIVE_ARRAY)-1))
+end_time = time.time()
+elapsed_time = (end_time-start_time)*1000
+print("With a running time of: " , f'{elapsed_time:.9f}',"ms")
